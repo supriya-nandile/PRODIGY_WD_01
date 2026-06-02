487fe77bed9c4bcf1a46f082c1c1fbfce28f9332
supriya-nandile <nandilesupriya@gmail.com>
2026-06-02 17:10:07 +0530

    Add scroll effect for navigation bar
    Implemented JavaScript functionality to detect page scrolling and dynamically change the navigation bar appearance, improving user experience and fulfilling internship task requirements.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Landing Page Loaded Successfully!");
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
